a962bae6129e0bc95fc36baa799d3a72797de530
warlock39 <warlock39@gmail.com>
2016-07-05 00:26:42 +0300

    Access API directly by host name, remove query path part

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@
 $filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER['REQUEST_URI'])) {
     return false;
-} elseif (0 === strpos($_SERVER['REQUEST_URI'], '/api')) {
-    return require_once __DIR__.'/api.php';
 }
+//elseif (0 === strpos($_SERVER['REQUEST_URI'], '/api')) { }
+return require_once __DIR__.'/api.php';
