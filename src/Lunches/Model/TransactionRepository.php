aa106c0e4ac30ec613e9ddfec2bf626765d5e5db
warlock39 <warlock39@gmail.com>
2016-08-14 03:36:48 +0300

    [Transactions] (API-81) TransactionRepository::findByUser() implemented

--- src/Lunches/Model/TransactionRepository.php
+++ src/Lunches/Model/TransactionRepository.php
@@ -9,4 +9,19 @@ use Doctrine\ORM\EntityRepository;
  */
 class TransactionRepository extends EntityRepository
 {
+    /**
+     * @param string $user
+     * @return array
+     */
+    public function findByUser($user)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select(['t'])
+            ->from('Lunches\Model\Transaction', 't')
+            ->where('t.user = :user')
+        ;
+        $qb->setParameter('user', $user);
+
+        return $qb->getQuery()->getResult();
+    }
 }
